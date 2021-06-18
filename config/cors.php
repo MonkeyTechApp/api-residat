dcc11cb6de0711154f82b7860aa4a1b306c98ded
Feussom Monkey <devronaldjunior@gmail.com>
2021-06-18 02:22:58 +0100

    Cors added

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie','http://localhost:8080'],
 
     'allowed_methods' => ['*'],
 
